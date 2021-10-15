8d61b6e26eefb7d11199b2f724b6eda47f3ce5c2
Andrey Sinetskiy <siniy.85@mail.ru>
2021-10-15 12:43:31 +0300

    add label

--- WebKit/ViewController.swift
+++ WebKit/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 0, y: 100, width: 400, height: 200)
+        label.text = "Create by Andrey"
+        label.font = UIFont(name: "Zapfino" , size: 30)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 70)
+        button.setTitle("Google", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Zapfino" , size: 30)
+        button.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        button.setTitleColor(.yellow, for: .normal)
+        button.layer.cornerRadius = 10
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addGradient()
+        view.addSubview(button)
+        view.addSubview(label)
+        button.addTarget(self, action: #selector(didTabButton), for: .touchUpInside)
+        button.center = view.center
+        
     }
 
-
+    @objc func didTabButton() {
+        if let url = URL(string: "http://www.google.com") {
+            let safariVC = SFSafariViewController(url: url)
+            present(safariVC, animated: true, completion: nil)
+        }
+    }
+    private func addGradient() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.yellow.cgColor, UIColor.white.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        gradient.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        view.layer.insertSublayer(gradient, at: 0)
+    }
 }
 
