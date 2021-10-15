ab8ad15d7e3564c0db42b7ccc4c91250b7257f84
Andrey Sinetskiy <siniy.85@mail.ru>
2021-10-15 11:42:32 +0300

    Initial Commit

---
+++ WebKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebKit
+//
+//  Created by 1 on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
